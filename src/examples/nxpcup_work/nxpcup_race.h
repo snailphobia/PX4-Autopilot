c032bbab61e64721fc3e719fafd827e04c7d113a
Landon Haugh <landon.haugh@nxp.com>
2021-03-10 10:54:51 -0600

    Fix wheel going backwards due to mixer

--- src/examples/nxpcup_work/nxpcup_race.h
+++ src/examples/nxpcup_work/nxpcup_race.h
@@ -43,9 +43,9 @@
 #include <px4_defines.h>
 #include <uORB/topics/pixy_vector.h>
 
-#define SPEED_FAST	0.3f
-#define SPEED_NORMAL	0.2f
-#define SPEED_SLOW	0.1f
+#define SPEED_FAST	0.15f
+#define SPEED_NORMAL	0.1f
+#define SPEED_SLOW	0.05f
 #define SPEED_STOP	0.0f
 
 struct roverControl {
